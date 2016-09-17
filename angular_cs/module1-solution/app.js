db6a5c796966f961c6a8b0b5433a4692bf18bf60
Sachin <imsachiniit@gmail.com>
2016-09-17 22:13:52 +0530

    Changing message

--- angular_cs/module1-solution/app.js
+++ angular_cs/module1-solution/app.js
@@ -8,27 +8,29 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   
   $scope.lunch_menu = "";
-  $scope.fontcolor = {};
+  $scope.content="";
+  $scope.message="";
 
   $scope.check_count = function () {
   	
   	if($scope.lunch_menu=="") //handles when textbox is empty or containing spaces only
   	{
-  		$scope.lunch_menu = "Please enter data first";
-  		$scope.fontcolor = {"color":"red"};
+  		$scope.content = "Pleas enter data first";
+  		$scope.message = {"color":"red"};
+
   		return;
   	}
 
-  	$scope.fontcolor = {"color":"green"};
 	var menus = $scope.lunch_menu.split(',');
-
+	$scope.message = {"color":"green"};
   	if(menus.length <= 3)
   	{
-  		$scope.lunch_menu = "Enjoy!";		
+  		$scope.content = "Enjoy!";		
   	}
   	else
   	{
-  		$scope.lunch_menu = "Too much!";
+
+  		$scope.content = "Too much!";
   	}
 
   };
